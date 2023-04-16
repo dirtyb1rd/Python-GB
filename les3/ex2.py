f19427e47864d75bdb521427fd071102d4cb1818
dirtyb1rd <yeskov777.888@gmail.com>
2023-04-16 23:03:08 +0300

    done second task

---
+++ les3/ex2.py
@@ -0,0 +1,31 @@
+mainArr = []
+sizeArr = int(input('Укажите длину массива: '))
+
+# Заполяем массив
+for i in range(sizeArr):
+    addItem = int(input('Заполните массив: '))
+    mainArr.append(addItem)
+
+userNum = int(input('Введите число: '))
+
+# Создаем массив с расстояниями до указанного числа
+distArr = []
+distBtwNum = 0
+for i in range(len(mainArr)):
+    distBtwNum = abs(userNum - mainArr[i])
+    distArr.append(distBtwNum)
+print(mainArr)
+print(distArr)
+# Ищем в массиве с расстояниями наименьшее значение
+
+maxDist = 0
+for i in range(len(distArr)):
+    if (distArr[i] > maxDist):
+        maxDist = distArr[i]
+
+lowDist = 0
+for i in range(len(distArr)):
+    if (distArr[i] < maxDist):
+        lowDist = i
+
+print('Ближайшее число к числу', userNum, '- это', mainArr[lowDist])
